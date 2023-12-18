d1f1593b9ec98bac5ee2161696466825d97ef074
Evandani Giantino Rafif <69229349+Froyotive@users.noreply.github.com>
2023-12-19 05:38:25 +0700

    Menyesuaikan Dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -39,8 +39,6 @@
                     <li class="nav-item">
                         <a class="nav-link" href="#">Menu</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">Login</a>
                 </ul>
             </div>
         </div>
@@ -340,37 +338,36 @@
     </div>
 
 
-
     <!-- Bootstrap JS and dependencies -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-</body>
-<footer style="background-color: #265171; color: #ffffff; padding: 20px;">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <h4>COFFSIDE</h4>
-                <p>Our Location</p>
-                <p>Kantin Vokasi UI, Kukusam Kecamatan Beji, Kota Depok, Jawa Barat 16245</p>
-                <a href="https://www.instagram.com/coffside.id/" target="_blank">
-                    <img src="{{ asset('images/logo/instagram-logo.png') }}" alt="Instagram" width="30" height="30">
-                </a>
-                <a href="https://www.tiktok.com/@coffside.id" target="_blank">
-                    <img src="{{ asset('images/logo/tiktok-logo.png') }}" alt="TikTok" width="30" height="30">
-                </a>
-            </div>
-            <div class="col-md-8">
-                <div class="row">
-                    <div class="col-md-4">
-                    </div>
-                    <div class="col-md-4">
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+
+    <footer style="background-color: #265171; color: #ffffff; padding: 20px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <h4>COFFSIDE</h4>
+                    <p>Our Location</p>
+                    <p>Kantin Vokasi UI, Kukusam Kecamatan Beji, Kota Depok, Jawa Barat 16245</p>
+                    <a href="https://www.instagram.com/coffside.id/" target="_blank">
+                        <img src="{{ asset('images/logo/instagram-logo.png') }}" alt="Instagram" width="30" height="30">
+                    </a>
+                    <a href="https://www.tiktok.com/@coffside.id" target="_blank">
+                        <img src="{{ asset('images/logo/tiktok-logo.png') }}" alt="TikTok" width="30" height="30">
+                    </a>
+                </div>
+                <div class="col-md-8">
+                    <div class="row">
+                        <div class="col-md-4">
+                        </div>
+                        <div class="col-md-4">
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</footer>
+    </footer>
 
 
 </html>
