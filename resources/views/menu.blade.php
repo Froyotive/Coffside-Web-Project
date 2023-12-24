317551a5d092dc3a80667ac4ce28a24e6b023c98
Evandani Giantino Rafif <69229349+Froyotive@users.noreply.github.com>
2023-12-24 18:19:20 +0700

    Update menu.blade.php

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -41,6 +41,9 @@
                     <li class="nav-item">
                         <a class="nav-link active" href="#">Menu</a>
                     </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('login') }}">Login</a>
+                    </li>
                 </ul>
             </div>
         </div>
