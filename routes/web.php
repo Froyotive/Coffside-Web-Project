b4e86614d0511d99b9b60715d402eb9874af7bdd
Evandani Giantino Rafif <69229349+Froyotive@users.noreply.github.com>
2023-12-29 02:05:15 +0700

    Fitur Login Admin

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -44,4 +44,21 @@ Route::get('/banner-2', function (){
 
 Route::get('/banner-3', function (){
     return view('banner.banner-3');
+});
+
+Route::controller(AuthController::class)->group(function () {
+    Route::get('register', 'register')->name('register');
+    Route::post('register', 'registerSave')->name('register.save');
+  
+    Route::get('login', 'login')->name('login');
+    Route::post('login', 'loginAction')->name('login.action');
+  
+    Route::get('logout', 'logout')->middleware('auth')->name('logout');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+ 
 });
