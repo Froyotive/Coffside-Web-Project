7f4f67a98ce47fe1e4b62c09893f922948f7f085
Evandani Giantino Rafif <69229349+Froyotive@users.noreply.github.com>
2023-12-31 15:09:40 +0700

    Update Stock

--- resources/views/stocks/edit.blade.php
+++ resources/views/stocks/edit.blade.php
@@ -96,12 +96,18 @@
                         @csrf
                         @method('PUT')
                         <div class="mb-3">
+                            <label for="menu_id" class="form-label">Menu:</label>
+                            <input type="hidden" name="menu_id" value="{{ $stock->menu_id }}">
+                            <p>{{ $stock->menu->name }}</p>
+                        </div>
+                        <div class="mb-3">
                             <label for="quantity" class="form-label">Quantity:</label>
                             <input type="number" class="form-control" id="quantity" name="quantity"
                                 value="{{ $stock->quantity }}" required>
                         </div>
                         <button type="submit" class="btn btn-primary">Update</button>
                     </form>
+
                 </div>
             </main>
             <a href="#" class="theme-toggle">
